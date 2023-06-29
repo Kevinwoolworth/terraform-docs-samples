32c9a2a65c3eda9ba3a9ad53ea0ff23579d485ca
Kevinwoolworth <87849802+Kevinwoolworth@users.noreply.github.com>
2023-06-29 22:56:19 +1000

    update script

--- eventarc/basic/update_pubsub_retention.sh
+++ eventarc/basic/update_pubsub_retention.sh
@@ -11,20 +11,20 @@ subscriptions=$(gcloud pubsub subscriptions list --project=$project_id --format=
 for subscription in $subscriptions; do
  # Get the existing message retention duration
   retention_duration=$(gcloud pubsub subscriptions describe $subscription --project=$project_id --format="value(messageRetentionDuration)")
-
+  echo "-------------------------------------------------------"
   # Print the existing retention duration
-  echo "Existing message retention duration for $subscription: $retention_duration"
+  echo "Existing message retention duration for '$subscription': '$retention_duration'"
 
   # Check if the retention duration is not already set to 7 days
   if [[ $retention_duration != "604800s" ]]; then
-    echo "Updating message retention duration for $subscription to 7 days..."
+    echo "Updating message retention duration for '$subscription' to 7 days..."
 
     # Update the message retention duration
     gcloud pubsub subscriptions update $subscription --project=$project_id --message-retention-duration=604800s
 
-    echo "Message retention duration updated for $subscription"
+    echo "Message retention duration updated for '$subscription'"
   else
-    echo "Retention duration already set to 7 days for $subscription"
+    echo "Retention duration already set to 7 days for '$subscription'"
   fi
 
   echo "======================================================"
