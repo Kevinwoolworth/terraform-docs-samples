611b02a002b1cb419a2bf984225ffc84c6c185ca
Kevinwoolworth <87849802+Kevinwoolworth@users.noreply.github.com>
2023-06-29 22:31:10 +1000

    upate

--- eventarc/basic/update_pubsub_retention.sh
+++ eventarc/basic/update_pubsub_retention.sh
@@ -2,12 +2,14 @@
 
 set -xeou pipefail
 
-project_id="your-project-id"
+project_id="gcp-wow-rwds-api-poc-dev"
 
 # Get a list of existing Pub/Sub subscriptions
 subscriptions=$(gcloud pubsub subscriptions list --project=$project_id --format="value(name)")
 
 # Loop through each subscription and update its message retention duration
 for subscription in $subscriptions; do
+  echo "========== $subscription ========= "
   gcloud pubsub subscriptions update $subscription --project=$project_id --message-retention-duration=604800s
+  echo "================================== "
 done
