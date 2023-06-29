419a44375fafe373462803781f61c68bb5992169
Kevinwoolworth <87849802+Kevinwoolworth@users.noreply.github.com>
2023-06-29 22:33:57 +1000

    update script

--- eventarc/basic/update_pubsub_retention.sh
+++ eventarc/basic/update_pubsub_retention.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xeou pipefail
+set -eou pipefail
 
 project_id="gcp-wow-rwds-api-poc-dev"
 
@@ -9,7 +9,21 @@ subscriptions=$(gcloud pubsub subscriptions list --project=$project_id --format=
 
 # Loop through each subscription and update its message retention duration
 for subscription in $subscriptions; do
-  echo "========== $subscription ========= "
-  gcloud pubsub subscriptions update $subscription --project=$project_id --message-retention-duration=604800s
-  echo "================================== "
+ # Get the existing message retention duration
+  retention_duration=$(gcloud pubsub subscriptions describe $subscription --project=$project_id --format="value(messageRetentionDuration)")
+
+  # Print the existing retention duration
+  echo "Existing message retention duration for $subscription: $retention_duration"
+
+  # Check if the retention duration is not already set to 7 days
+  if [[ $retention_duration != "604800s" ]]; then
+    echo "Updating message retention duration for $subscription to 7 days..."
+
+    # Update the message retention duration
+    gcloud pubsub subscriptions update $subscription --project=$project_id --message-retention-duration=604800s
+
+    echo "Message retention duration updated for $subscription"
+  else
+    echo "Retention duration already set to 7 days for $subscription"
+  fi
 done
