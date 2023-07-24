fc7dd505a70f4ee34746a5fbece561c4c65d9218
Kevinwoolworth <87849802+Kevinwoolworth@users.noreply.github.com>
2023-07-25 08:57:45 +1000

    test

---
+++ log/main.tf
@@ -0,0 +1,147 @@
+provider "google" {
+  region = "australia-southeast1-a"
+}
+
+resource "google_monitoring_notification_channel" "email-me" {
+  project      = "gcp-wow-rwds-data-poc-dev"
+  display_name = "Email Me"
+  type         = "email"
+  labels = {
+    email_address = "me@mycompany.com"
+  }
+}
+
+resource "google_monitoring_alert_policy" "workflows" {
+  project      = "gcp-wow-rwds-data-poc-dev"
+  display_name = "Workflows alert policy"
+  combiner     = "OR"
+  conditions {
+    display_name = "Error condition"
+    condition_matched_log {
+      filter = "resource.type=\"workflows.googleapis.com/Workflow\" severity=ERROR"
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email-me.name]
+  alert_strategy {
+    notification_rate_limit {
+      period = "300s"
+    }
+  }
+}
+
+#resource "google_monitoring_alert_policy" "alert_policy" {
+#  project      = "gcp-wow-rwds-data-poc-dev"
+#  display_name = "My Alert Policy"
+#  combiner     = "OR"
+#  conditions {
+#    display_name = "test condition"
+#    condition_threshold {
+#      filter          = "metric.type=\"logging.googleapis.com/user/test-metrics\" AND resource.type=\"cloud_run_revision\""
+#      duration        = "600s"
+#      comparison      = "COMPARISON_GT"
+#      threshold_value = 1
+#    }
+#  }
+#
+#  user_labels = {
+#    foo = "bar"
+#  }
+#}
+
+#resource "google_logging_metric" "my_log_metrics" {
+#  project = "gcp-wow-rwds-data-poc-dev"
+#  name = "my-log-metric"
+#  filter = "..."
+#  description = "..."
+#  metric_descriptor {
+#    metric_kind = "..."
+#    value_type = "..."
+#  }
+#}
+
+resource "google_logging_metric" "logging_metric" {
+  project = "gcp-wow-rwds-data-poc-dev"
+  name    = "my-(custom)/metric"
+  filter  = "resource.type=gae_app AND severity>=ERROR"
+  metric_descriptor {
+    metric_kind = "DELTA"
+    value_type  = "DISTRIBUTION"
+    unit        = "1"
+    labels {
+      key         = "mass"
+      value_type  = "STRING"
+      description = "amount of matter"
+    }
+    labels {
+      key         = "sku"
+      value_type  = "INT64"
+      description = "Identifying number for item"
+    }
+    display_name = "My metric"
+  }
+  value_extractor = "EXTRACT(jsonPayload.request)"
+  label_extractors = {
+    "mass" = "EXTRACT(jsonPayload.request)"
+    "sku"  = "EXTRACT(jsonPayload.id)"
+  }
+  bucket_options {
+    linear_buckets {
+      num_finite_buckets = 3
+      width              = 1
+      offset             = 1
+    }
+  }
+}
+
+#resource "google_monitoring_alert_policy" "my_policy" {
+#  project = "gcp-wow-rwds-data-poc-dev"
+#  display_name = "my-policy"
+#  combiner = "OR"
+#  conditions {
+#    display_name = "my-policy"
+#    condition_threshold {
+#      filter = "metric.type=\"logging.googleapis.com/user/my-log-metric\" AND resource.type=\"cloud_composer_environment\""
+#    }
+#  }
+#}
+
+#resource "google_compute_firewall" "terraform" {
+#  name    = "terraform-firewall"
+#  network = "default"
+#
+#  allow {
+#    protocol = "icmp"
+#  }
+#
+#  allow {
+#    protocol = "tcp"
+#    ports    = ["80", "8800", "443", "22"]
+#  }
+#}
+
+resource "google_compute_firewall" "default" {
+  project = "gcp-wow-rwds-data-poc-dev"
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_network" "default" {
+  project = "gcp-wow-rwds-data-poc-dev"
+  name    = "test-network"
+}
---
+++ log/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-west1-a" # Oregon
+}
+
+variable "project-name" {
+  default = "gsoc-172407"
+}
+
+variable "subnetwork-region" {
+  default = "us-west1"
+}
+
+variable "network" {
+  default = "ovirt-network"
+}
